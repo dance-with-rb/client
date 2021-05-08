72931bde5f4c86aef96501b78e0c0eb599c41a06
bolam-o <bolam@kakao.com>
2021-05-09 02:24:56 +0900

    chore: Add postcss webpack configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,10 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.postcss$/,
+      use: ['vue-style-loader', 'style-loader', 'css-loader', 'postcss-loader'],
+    });
     config.resolve.plugins.push(new TsconfigPathsPlugin({}));
     return config;
   },
