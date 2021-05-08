a21211405ccad1926bdb4cc4dec22c671af14ee7
bolam-o <bolam@kakao.com>
2021-05-09 02:03:39 +0900

    test: Update Button component test

--- src/components/atom/Button/__tests__/Button.spec.ts
+++ src/components/atom/Button/__tests__/Button.spec.ts
@@ -3,11 +3,11 @@ import { shallowMount } from '@vue/test-utils';
 import Button from '../Button.vue';
 
 describe('Button.vue', () => {
-  const wrapper = shallowMount(Button);
-
   it('정해진 Prop에 따라 버튼 텍스트가 변경되어야 함', () => {
     const text = '테스트';
-    wrapper.setProps({ text });
+    const wrapper = shallowMount(Button, {
+      propsData: { text },
+    });
 
     expect(wrapper.text()).toBe(text);
   });
