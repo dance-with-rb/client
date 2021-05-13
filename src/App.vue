2b3bf886c80a95b2f748e35765afa88ab0f50c04
bolam-o <bolam@kakao.com>
2021-05-14 00:35:09 +0900

    feat: Update loading state with store getter

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
+    <NavigationBar v-if="isAdminPage" />
     <router-view />
-    <Loading :isLoading="this.$store.state.common.isLoading" />
+    <Loading :isLoading="isLoading" />
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 import Loading from '@organisms/Loading';
+import NavigationBar from '@organisms/NavigationBar';
 
-@Component({ components: { Loading } })
-export default class App extends Vue {}
+@Component({ components: { Loading, NavigationBar } })
+export default class App extends Vue {
+  get isLoading(): boolean {
+    return this.$store.getters['common/getLoadingState'];
+  }
+
+  get isAdminPage(): boolean {
+    return this.$route.path.includes('/admin');
+  }
+}
 </script>
