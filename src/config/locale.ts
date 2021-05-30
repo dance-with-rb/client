a0ec5f0cc3690fdbf45ee1d8cfe0bb899c2dcd39
bolam-o <bolam@kakao.com>
2021-05-30 13:56:21 +0900

    feat: Update validate locale config

--- src/config/locale.ts
+++ src/config/locale.ts
@@ -4,9 +4,14 @@ export default {
       attributes: {
         username: '아이디',
         password: '비밀번호',
+        vocTitle: '문의명',
+        vocContent: '문의내용',
+        vocAuthor: '작성자명',
+        replyEmail: '이메일 주소',
       },
       messages: {
-        required: (field: string): string => `${field}를 입력해주세요.`,
+        required: (field: string): string => `${field}을(를) 입력해주세요.`,
+        email: (): string => `이메일 형식을 확인해주세요.`,
       },
     },
   },
